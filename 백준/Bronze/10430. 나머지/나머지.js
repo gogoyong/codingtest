8c9160d66a9627ccec73dd9b12d30bd521a7aa4c
gogoyong <90438815+gogoyong@users.noreply.github.com>
2022-06-09 12:19:24 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,11 @@
+let fs=require('fs')
+let input=fs.readFileSync('dev/stdin').toString().split(' ').map(Number)
+
+const a = input[0]
+const b = input[1]
+const c = input[2]
+
+console.log((a+b)%c)
+console.log(((a%c)+(b%c))%c)
+console.log((a*b)%c)
+console.log(((a%c)*(b%c))%c)
