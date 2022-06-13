eb89ad301a5eb0c2a1b35b41901ede2386002ec7
gogoyong <90438815+gogoyong@users.noreply.github.com>
2022-06-13 21:42:57 +0900

    [Bronze III] Title: 최소, 최대, Time: 436 ms, Memory: 94508 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,7 +1,7 @@
 let fs=require('fs')
 let input=fs.readFileSync('dev/stdin').toString().split('\n')
 let array = input[1].split(' ').map(Number)
-let max = Math.max.apply(null,array)
-let min = Math.min.apply(null,array)
+let max = Math.max(...array)
+let min = Math.min(...array)
 
 console.log(`${min} ${max}`)
