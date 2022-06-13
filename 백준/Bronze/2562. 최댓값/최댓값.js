d3c21f736be8451be1448b9ccaf14870659b0dbe
gogoyong <90438815+gogoyong@users.noreply.github.com>
2022-06-13 21:24:41 +0900

    [Bronze II] Title: 최댓값, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,7 @@
+let fs=require('fs')
+let input=fs.readFileSync('dev/stdin').toString().split('\n').map(Number)
+
+const max = Math.max.apply(null,input)
+const index = input.indexOf(max)+1
+
+console.log(`${max}\n${index}`)
