5ed47d21738b02aecd9a10ab9e9e695ca75b5b3c
Ryu Yong Wook <90438815+gogoyong@users.noreply.github.com>
2022-07-12 11:00:14 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1332 ms, Memory: 166352 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,12 @@
+let fs = require('fs')
+let input = fs.readFileSync('./dev/stdin').toString().split('\n')
+
+let t = Number(input[0])
+let answer = ''
+for (i=1;i<=t;i++){
+  let data = input[i].split(' ')
+  let a = Number(data[0])
+  let b = Number(data[1])
+  answer += a + b + '\n'
+}
+console.log(answer)
