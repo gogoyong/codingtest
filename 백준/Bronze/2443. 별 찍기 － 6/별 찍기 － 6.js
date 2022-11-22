eb5ae68b0147f9277dc1636f2b4bb63aabd5ebee
Ryu YongWook <90438815+gogoyong@users.noreply.github.com>
2022-11-22 20:52:40 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 128 ms, Memory: 9544 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin').toString().trim()
+
+const n = Number(input)
+
+for (i=n;i>0;i--) {
+  console.log(' '.repeat(n-i) + '*'.repeat(2*i-1))
+}
