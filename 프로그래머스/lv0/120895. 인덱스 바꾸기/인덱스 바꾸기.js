95f52d7e2638575aaa985248ce46ace4a6345ff7
Ryu YongWook <90438815+gogoyong@users.noreply.github.com>
2022-11-25 19:49:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,7 @@
+function solution(my_string, num1, num2) {
+    const array = my_string.split('')
+    const temp = array[num1]
+    array[num1] = array[num2]
+    array[num2] = temp
+    return array.join('');
+}
